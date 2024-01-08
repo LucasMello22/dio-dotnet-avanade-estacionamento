dfc4f5b16602a6107c022085ae91c10fd6eb494c
Lucas Mello22 <lucas.s.mello@outlook.com>
2024-01-08 09:00:57 -0300

    finaly project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using dotnet.Models;
+
+Console.WriteLine("Hello, World!");
+
+Pessoa p1 = new Pessoa ();
+
+p1.Nome = "Lucas";
+p1.Idade = 24;
+p1.Apresentar();
